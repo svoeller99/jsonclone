658d47d5a27e9ccd20fcc2c367c0529b32c5ad4c
Sean Voeller <sean.voeller@gmail.com>
2015-05-27 19:56:58 -0700

    adding simple type registry mechanism

--- test/jsonclone_test.js
+++ test/jsonclone_test.js
@@ -33,6 +33,18 @@ MyStuff.prototype.sayMyName = function() {
 
 var testObj = new MyStuff('sean', 'voeller');
 
+var cogChirp;
+
+function Cog(id) {
+  this.id = id;
+}
+Cog.prototype.chirp = function() {
+  cogChirp = "Hello I am cog " + this.id;
+};
+function Wheel(){
+
+}
+
 exports.jsonclone = {
   setUp: function(done) {
     // setup here
@@ -55,6 +67,20 @@ exports.jsonclone = {
     test.expect(1);
     var clone = jsonclone.parseOLD(jsonclone.stringifyOLD(testObj));
     test.ok(typeof clone.__jsonclone__ === 'undefined');
+    test.done();
+  },
+
+  // new stuff
+  'jsonclone - has a registry that stores and serves up types': function(test) {
+    jsonclone.types.add(Cog);
+    var cog = jsonclone.types.get('Cog');
+    test.equal(Cog, cog, 'should be the same constructor function');
+    new cog(1).chirp();
+    test.equal(cogChirp, "Hello I am cog 1");
+
+    jsonclone.types.add('myWheel', Wheel);
+    var wheel = jsonclone.types.get('myWheel');
+    test.equal(Wheel, wheel, 'should be the same constructor function when registered with specific name');
     test.done();
   }
 };
